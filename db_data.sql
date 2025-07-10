07c6ec377f159e706045b66ed634dac3123898cb
Gabriele Livrieri <gabriele.livrieri.005@gmail.com>
2025-07-10 13:02:24 +0200

    FILE DATABASE
    schema e data del database da usare con il progetto

---
+++ db_data.sql
@@ -0,0 +1,13 @@
+USE progetto;
+
+INSERT INTO utente (nome, cognome, telefono, email) VALUES
+('Mario', 'Rossi', 'mario.rossi@example.com', '3201234567'),
+('Luigi', 'Verdi', 'luigi.verdi@example.com', '3279876543'),
+('Anna', 'Bianchi', 'anna.bianchi@example.com', '3291112233'),
+('Giulia', 'Neri', 'giulia.neri@example.com', '3315566778'),
+('Francesco', 'Russo', 'francesco.russo@example.com', '3338889900'),
+('Paolo', 'Ferrari', 'paolo.ferrari@example.com', '3344455667'),
+('Chiara', 'Romano', 'chiara.romano@example.com', '3356677889'),
+('Davide', 'Conti', 'davide.conti@example.com', '3367788990'),
+('Sara', 'Marino', 'sara.marino@example.com', '3379988776'),
+('Luca', 'Gallo', 'luca.gallo@gmail.com', '3385566442')
